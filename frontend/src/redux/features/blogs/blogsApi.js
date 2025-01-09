648101ee83e90f03e0585708099816b4fcae17b9
eyasin121 <arafathossain000121@gmail.com>
2025-01-09 13:43:46 +0600

    Update blogsApi.js

--- frontend/src/redux/features/blogs/blogsApi.js
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const blogApi = createApi({
     reducerPath: 'blogsApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000/api',
+        baseUrl: 'https://techno-blogger.vercel.app/api',
         credentials: 'include',
     }),
     tagTypes: ['Blog'],
