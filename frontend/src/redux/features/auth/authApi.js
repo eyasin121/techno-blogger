995226ea303c663190f66147c1060ac7329d563a
eyasin121 <arafathossain000121@gmail.com>
2025-01-09 13:43:30 +0600

    Update authApi.js

--- frontend/src/redux/features/auth/authApi.js
+++ frontend/src/redux/features/auth/authApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000/api/user',
+        baseUrl: 'https://techno-blogger.vercel.app/api/user',
         credentials: 'include',
     }),
     endpoints: (builder) => ({
